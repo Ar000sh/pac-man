e0fe22d13095c0ca47a0e9e43eafcf82c2f287f5
Tariq <tariq.al-arashi@stud.th-luebeck.de>
2022-09-01 12:49:42 +0200

    adding pvc

--- backend/modales.py
+++ backend/modales.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 # import time
 from sqlalchemy import func 
-from modales import Highscore
+
 # from sqlalchemy import event
 # from sqlalchemy.engine import Engine
 # from sqlite3 import Connection as SQLite3Connection
@@ -18,8 +18,38 @@ from modales import Highscore
 
 app = Flask(__name__)
 CORS(app) # This allows CORS for all domains on all routes
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password123!@10.152.183.34:3306/demo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password123!@mariadb:3306/demo'
 db = SQLAlchemy(app)
+
+# engine = create_engine("mysql+pymysql://root:Password123!@localhost:3306/database")
+# from sqlalchemy import event
+# from sqlalchemy.engine import Engine
+# from sqlite3 import Connection as SQLite3Connection
+# @event.listens_for(Engine, "connect")
+# def _set_sqlite_pragma(dbapi_connection, connection_record):
+#     if isinstance(dbapi_connection, SQLite3Connection):
+#         cursor = dbapi_connection.cursor()
+#         cursor.execute("PRAGMA foreign_keys=ON;")
+#         cursor.close()
+
+
+
+
+class Highscore(db.Model):#1
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    highscore = db.Column(db.Integer, nullable=False)
+    # Erzeugt aus einer Instanz einer Klasse ein Json Format
+    # Lazy führt wirkliche Änderung erst ab Commit aus
+    def to_json(self):
+        json_post = {
+            'id': self.id,
+            'highscore': self.highscore
+        }
+        return json_post
+
+# db.drop_all()
+db.create_all()
+
 
 @app.route('/highscores', methods=['GET'])
 def get_highscores():
